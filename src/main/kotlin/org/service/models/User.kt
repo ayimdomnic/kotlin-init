162cb634cb974d1b487ecb0fd39b692f6d528f30
Odhiambo Dormnic <d.odhiambo@carepay.co.ke>
2019-10-16 01:01:45 +0300

    Yaaay, Hope I find a proper ORM

---
+++ src/main/kotlin/org/service/models/User.kt
@@ -0,0 +1,3 @@
+package org.service.models
+
+data class User(val name: kotlin.String, val email: kotlin.String, val id: Int)
---
+++ src/test/kotlin/org/service/HelloTest.kt
@@ -0,0 +1,8 @@
+package org.service
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
